a383c703192a8f8eaccd27369eaffb1ceb0ff3d5
Max <mkondel@gmail.com>
2018-03-20 14:03:48 -0400

    added 1st working build

---
+++ build/webserver.js
@@ -0,0 +1,7 @@
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var port = process.env.PORT || 80
+connect().use(serveStatic(__dirname)).listen(port, function(){
+    console.log('Server running on '+port);
+});
+
