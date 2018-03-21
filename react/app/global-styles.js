53cd35664b72d95163b5463a5ed39fcb7474a763
Max <mkondel@gmail.com>
2018-03-20 20:41:57 -0400

    simplified react

--- react/app/global-styles.js
+++ react/app/global-styles.js
@@ -10,7 +10,6 @@ injectGlobal`
 
   body {
     font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-    color: white;
   }
 
   body.fontLoaded {
@@ -18,8 +17,7 @@ injectGlobal`
   }
 
   #app {
-    background-color: #1a1914;
-    /*background-color: #fafafa;*/
+    background-color: #fafafa;
     min-height: 100%;
     min-width: 100%;
   }
